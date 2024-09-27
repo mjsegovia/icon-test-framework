a506afa1d770d7945c35c0ca478de41209ae3136
mjsegovia <mj.segovia22@gmail.com>
2024-09-27 09:23:38 +0200

    added settings and common classes on the core project

---
+++ IconTestFramework.Core/Config/Configurator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace IconTestFramework.Core.Config
+{
+    public static class Configurator
+    {
+        public static IConfigurationRoot Config = new ConfigurationBuilder()
+                                                    .SetBasePath(Directory.GetCurrentDirectory() + @"..\..\..\..\..\IconTestFramework.Core\Config")
+                                                    .AddJsonFile("appsettings.json")
+                                                    .Build();
+
+        public static string ApiUrl => GetAppSettings("Uri");
+        public static string BaseUrl => GetAppSettings("base_url");
+        public static string LoginUrl => GetAppSettings("login_url");
+
+        private static string GetAppSettings(string name) => Config["AppSettings:" + name];
+    }
+}
---
+++ IconTestFramework.Core/Domain/User.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace IconTestFramework.Core.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("avatar")]
+        public string Avatar { get; set; }
+    }
+
+    public class UserResponse
+    {
+        public int Page { get; set; }
+
+        [JsonPropertyName("per_page")]
+        public int PerPage { get; set; }
+
+        public int Total { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+
+        public List<User> Data { get; set; }
+    }
+}
